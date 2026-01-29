fff36406cd88dc93b3599bd0114022fa9e6308aa
Akalya01 <aakalya603@gmail.com>
2026-01-29 12:04:38 +0530

    updated backend

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -2,6 +2,7 @@ import app from "./app.js";
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
+
